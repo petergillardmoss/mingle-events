7d8664643575bbc6ac682b4de04f6e9747f82227
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-18 16:22:06 +0100

    add changes to entry

--- test/mingle_events/change_test.rb
+++ test/mingle_events/change_test.rb
@@ -5,6 +5,7 @@ module MingleEvents
 
     def test_equality
       assert_equal(Change.new('old value'), Change.new('old value'))
+      assert_not_equal(Change.new('old value'), Change.new('different value'))
     end
 
   end
