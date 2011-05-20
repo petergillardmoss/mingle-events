fc764d3d91bd6f5ffd5668d1d9fcf4689cb02656
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-20 15:33:03 +0100

    add changes to entry: refactor

--- lib/mingle_events/change.rb
+++ lib/mingle_events/change.rb
@@ -1,24 +1,35 @@
 module MingleEvents
 
-  class Change
-
+  module ValueChange
     # The change's old value
     attr_reader :old_value
 
     # The change's new value
     attr_reader :new_value
 
+    def initialize old_value, new_value
+      @old_value = old_value
+      @new_value = new_value
+    end
+
+    def same_values_as(other)
+      other.old_value == self.old_value && other.new_value == self.new_value
+    end
+  end
+
+  class Change
+    include ValueChange
+
     # The change's new value
     attr_reader :change_type
 
     def initialize change_type, old_value, new_value
       @change_type = change_type
-      @old_value = old_value
-      @new_value = new_value
+      super old_value, new_value
     end
 
     def ==(other)
-      other.is_a?(Change) && other.old_value == self.old_value && other.new_value == self.new_value && other.change_type == self.change_type
+      other.is_a?(Change) && other.change_type == self.change_type && same_values_as(other)
     end
 
     def hash
@@ -31,23 +42,18 @@ module MingleEvents
   end
 
   class PropertyChange
+    include ValueChange
+
     # The name of the property
     attr_reader :name
 
-    # The change's old value
-    attr_reader :old_value
-
-    # The change's new value
-    attr_reader :new_value
-
     def initialize old_value, new_value, name
-      @old_value = old_value
-      @new_value = new_value
       @name = name
+      super old_value, new_value
     end
 
     def ==(other)
-      other.is_a?(PropertyChange) && other.old_value == self.old_value && other.new_value == self.new_value
+      other.is_a?(PropertyChange) && same_values_as(other)
     end
 
     def hash
