cbb34a725cccb2fa4f389cf5c98037f80d886b71
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-20 16:38:23 +0100

    add changes to entry: refactor

--- lib/mingle_events/changes.rb
+++ lib/mingle_events/changes.rb
@@ -9,6 +9,9 @@ module MingleEvents
     def create_change change_element
       change_type = change_element.attribute('type').text
 
+      def change_element.old_value() at('./mingle:old_value').inner_text end
+      def change_element.new_value() at('./mingle:new_value').inner_text end
+
       changes_types = {
         'card-creation' => lambda { CardCreationChange.new },
         'card-deletion' => lambda { CardDeletionChange.new }
@@ -16,12 +19,10 @@ module MingleEvents
 
       return changes_types[change_type].call unless !changes_types[change_type]
 
-      old_value = change_element.at('./mingle:old_value').inner_text
-      new_value = change_element.at('./mingle:new_value').inner_text
-
-      return NameChange.new(old_value, new_value) if change_type == 'name-change'
-      return PropertyChange.new(old_value, new_value,
+      return NameChange.new(change_element.old_value, change_element.new_value) if change_type == 'name-change'
+      return PropertyChange.new(change_element.old_value, change_element.new_value,
                                 change_element.at('.//mingle:name').inner_text) if change_type == 'property-change'
     end
+
   end
 end
