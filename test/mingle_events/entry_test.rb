3063d3aad067f478cd1e702dc4e6dbc6e0a4e35c
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-20 12:16:09 +0100

    add changes to entry

--- test/mingle_events/entry_test.rb
+++ test/mingle_events/entry_test.rb
@@ -40,31 +40,6 @@ module MingleEvents
       )
     end
 
-    def test_parse_changes
-      element_xml_text = %{
-        <entry xmlns:mingle="http://www.thoughtworks-studios.com/ns/mingle">
-          <content type="application/vnd.mingle+xml">
-            <changes xmlns="http://www.thoughtworks-studios.com/ns/mingle">
-              <change type="name-change">
-                <old_value>Old name 1</old_value>
-                <new_value>New name 1</new_value>
-              </change>
-              <change type="property-change">
-                <old_value>Old name 2</old_value>
-                <new_value>New name 2</new_value>
-              </change>
-            </changes>
-          </content>
-        </entry>}
-      element = Nokogiri::XML(element_xml_text)
-
-      entry = Entry.new(element)
-      assert_equal(
-         [Change.new('name-change', 'Old name 1', 'New name 1'), Change.new('property-change', 'Old name 2', 'New name 2')],
-         entry.changes
-      )
-    end
-
     def test_parse_card_number_and_version
 
       # the links below contain intentionally nonsensical data so as to ensure
