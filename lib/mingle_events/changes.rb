41a04d2df3475d11a6b3bc462dbf1bcceeead4ed
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-23 13:13:04 +0100

    add changes to entry: refactor

--- lib/mingle_events/changes.rb
+++ lib/mingle_events/changes.rb
@@ -2,27 +2,34 @@ module MingleEvents
   module Changes
     # Any changes related to the event (if any).
     def changes
-      @changes ||= @entry_element.search('.//mingle:change').map(&method(:create_change)).reject { |e| e == nil}
+      @changes ||= @entry_element.search('.//mingle:change').map(&method(:create_change)).reject { |e| e == nil }
     end
 
     private
     def create_change change_element
       change_type = change_element.attribute('type').text
 
-      def change_element.old_value() at('./mingle:old_value').inner_text end
-      def change_element.new_value() at('./mingle:new_value').inner_text end
-      def change_element.property_name() at('.//mingle:name').inner_text end
+      change = Change.new(change_element)
 
       changes_types = {
         'card-creation' => lambda { CardCreationChange.new },
         'card-deletion' => lambda { CardDeletionChange.new },
-        'name-change' => lambda { NameChange.new(change_element.old_value, change_element.new_value) },
-        'property-change' => lambda { PropertyChange.new(change_element.old_value, change_element.new_value,
-                                change_element.property_name) }
+        'name-change' => lambda { NameChange.new(change.old_value, change.new_value) },
+        'property-change' => lambda { PropertyChange.new(change.old_value, change.new_value,
+                                change.property_name) }
       }
 
       return changes_types[change_type].call unless !changes_types[change_type]
     end
 
+    class Change
+      def initialize(element)
+        @element = element
+      end
+
+      def old_value() @element.at('./mingle:old_value').inner_text end
+      def new_value() @element.at('./mingle:new_value').inner_text end
+      def property_name() @element.at('.//mingle:name').inner_text end
+    end
   end
 end
